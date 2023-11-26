38f0497fdd4904cb1573a67107ba284ffa03243b
Ali Valizadeh <alivlizadeh1@github.com>
2023-11-26 23:25:04 +0330

    first commit

---
+++ src/main/java/com/tsm/ticketsalesmanagement/TicketSalesManagementApplication.java
@@ -0,0 +1,13 @@
+package com.tsm.ticketsalesmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketSalesManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketSalesManagementApplication.class, args);
+    }
+
+}
